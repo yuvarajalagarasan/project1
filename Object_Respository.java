1a8ff7477c639275edbae827ca4a250c4f4f3acc
yuvarajalagarasan <138753542+yuvarajalagarasan@users.noreply.github.com>
2023-08-22 14:54:30 +0530

    Add files via upload

---
+++ Object_Respository.java
@@ -0,0 +1,61 @@
+package webmathpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Object_Respository {
+	public static final By mathForEveryone = By.xpath("//a[@title='Math for Everyone']");
+	public static final By textOfMathForEveryone = By.xpath("//div[@class='d-childMainContLeftRow']/h1");
+	public static final By chooseMathHelpItemDropdown = By.xpath("//select[@id='topicItem']");
+	public static final By goButton = By.xpath("//div[@class='d-quickJumpRight']/a/img");
+	public static final By textFromHomePage=By.xpath("//div[@class='d-homeCopyRow']/h1");
+	public static final By helpTypingInYourMathProblems   = By.xpath("//a[@title='Help typing in your problem']");
+	public static final By figuringATip = By.xpath("(//font[@size='2'])[1]/a[text()='Figuring a tip']");
+	public static final By salePrice = By.xpath("(//font[@size='2'])[1]/a[text()='Sale price']");
+	public static final By windChill = By.xpath("(//font[@size='2'])[1]/a[text()='Wind Chill']");
+	public static final By countCoins = By.xpath("(//font[@size='2'])[1]/a[text()='Count Coins']");
+	public static final By lotteryOdds = By.xpath("(//font[@size='2'])[1]/a[text()='Lottery Odds']");
+	public static final By simpleInterest = By.xpath("(//font[@size='2'])[2]/a[text()='Simple Interest']");
+	public static final By compoundInterest = By.xpath("(//font[@size='2'])[2]/a[text()='Compound Interest']");
+	public static final By retirement = By.xpath("(//font[@size='2'])[2]/a[text()='Retirement']");
+	public static final By aLoan = By.xpath("(//font[@size='2'])[2]/a[text()='A loan']");
+	public static final By length = By.xpath("(//font[@size='2'])[3]/a[text()='Length']");
+	public static final By mass = By.xpath("(//font[@size='2'])[3]/a[text()='mass']");
+	public static final By area = By.xpath("(//font[@size='2'])[3]/a[text()='area']");
+	public static final By volume = By.xpath("(//font[@size='2'])[3]/a[text()='volume']");
+	public static final By speed = By.xpath("(//font[@size='2'])[3]/a[text()='speed']");
+	public static final By power = By.xpath("(//font[@size='2'])[3]/a[text()='power']");
+	public static final By temps = By.xpath("(//font[@size='2'])[3]/a[text()='Temps.']");
+	public static final By costOfElectricity = By.xpath("(//font[@size='2'])[4]/a[text()='Cost of electricity']");
+	public static final By weightOnAnotherPlanet = By.xpath("(//font[@size='2'])[5]/a[text()='Weight on Another Planet?']");
+	public static final By homeInFooter = By.xpath("//a[@title=\'WebMath Page\']");
+	public static final By contactUs= By.xpath("//a[@title=\'Contact WebMath']");
+	public static final By aboutWebMath= By.xpath("//a[@title=\'About WebMath\']");
+	public static final By whyWebMath= By.xpath("//a[@title=\"Why WebMath\"]");
+	public static final By websiteMap= By.xpath("//a[@title=\"Website Map Index\"]");
+	public static final By privacyPolicy= By.xpath("//a[@title=\"Privacy Policy\"]");
+	public static final By textFromContactPage= By.xpath("//h1[text()='Contact Information']");
+	public static final By textFromAboutWebMathPage= By.xpath("//h1[text()='What is WebMath?']");
+    public static final By textFromWhyWebMath= By.xpath("//h1[text()='Why Webmath?']");
+    public static final By textFromWebsiteMap= By.xpath("//h1[text()='Website Map']");
+    public static final By textFromPrivacyPolicy= By.xpath("//h1[text()='Privacy Policy']");
+    public static final By textFromTipCalculator= By.xpath("//h1[text()='Tip Calculator']");
+    public static final By textFromSalePriceCalculator= By.xpath("//h1[text()='Calculate the Sale Price of an Item on Sale']");
+    public static final By textFromWindChillTemperature= By.xpath("//h1[text()='Calculate Windchill Temperature']");
+    public static final By textFromCountCoinsPage= By.xpath("//h1[text()='Help with Counting U.S. Coins']");
+    public static final By textFromLotteryOddsPage= By.xpath("//h1[text()='Calculate Your Chance of Winning the Lottery']");
+    public static final By textFromSimpleInterestCalculator= By.xpath("//h1[text()='Simple Interest Calculator']");
+    public static final By textFromCompoundInterestCalculator= By.xpath("//h1[text()='Compound Interest Calculator']");
+    public static final By textFromRetirementPage= By.xpath("//h1[text()='Retirement Investment Calculator']");
+    public static final By textFromALoanPage= By.xpath("//h1[text()='Amortization Schedule for a Loan']");
+    public static final By textFromLengthPage= By.xpath("//h1[text()='Units Conversions on Lengths']");
+    public static final By textFromMassPage= By.xpath("//h1[text()='Units Conversions on Masses']");
+    public static final By textFromAreaPage= By.xpath("//h1[text()='Units Conversions on Areas']");
+    public static final By textFromVolumePage= By.xpath("//h1[text()='Units Conversions on Volumes']");
+    public static final By textFromSpeedPage= By.xpath("//h1[text()='Units Conversions on Speed']");
+    public static final By textFromPowerPage= By.xpath("//h1[text()='Units Conversions on Power']");
+    public static final By textFromTempsPage= By.xpath("//h1[text()='Convert temperature Scales']");
+    public static final By textFromCostOfElectricityPage= By.xpath("//h1[text()='Calculate the Costs to Use Electricity']");
+    public static final By textFromWeightOnAnotherPLanetCalculator= By.xpath("//h1[text()='Calculate Weight on Another Planet']");
+
+}
